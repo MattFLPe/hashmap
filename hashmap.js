3e3ac512f7e9fe4c008997c730a801c8b79c5bbe
Mateus Felipe <brasil.mateus748@gmail.com>
2024-05-06 23:12:49 -0300

    Create class HashMap and hash method that takes a key and produces a hash code with it

--- hashmap.js
+++ hashmap.js
@@ -1,1 +1,17 @@
-console.log(test)
+class HashMap {
+    
+    hash(key){
+        let hashCode = 0;
+      
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = primeNumber * hashCode + key.charCodeAt(i);
+        }
+        return hashCode;
+    }
+
+    set(key, value) {
+        
+    }
+
+}
