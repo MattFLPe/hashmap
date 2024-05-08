db29493eef281997e615754fd18baf337a1c7dd9
Mateus Felipe <brasil.mateus748@gmail.com>
2024-05-07 23:50:40 -0300

    Implement methods: set, get, has, remove, length, clear, keys, values, and entries

--- hashmap.js
+++ hashmap.js
@@ -1,4 +1,8 @@
 class HashMap {
+
+  constructor() {
+    this.map = {};
+}
     
     hash(key){
         let hashCode = 0;
@@ -11,7 +15,49 @@ class HashMap {
     }
 
     set(key, value) {
-        
+      this.map[key] = value;
+    }
+
+    get(key) {
+      return this.map[key];
+    }
+
+    has(key) {
+      return key in this.map;
+    }
+
+    remove(key) {
+      if (this.has(key)) {
+        delete this.map[key];
+        return true;
+      }
+      return false;
+    }
+
+    length() {
+      return Object.keys(this.map).length;
+    }
+
+    clear() {
+      this.map = {};
+    }
+
+    keys() {
+      return Object.keys(this.map);
+    }
+
+    values() {
+      return Object.values(this.map);
+    }
+
+    entries() {
+      return Object.entries(this.map);
     }
 
 }
+
+const myMap = new HashMap();
+myMap.set('a', 1);
+myMap.set('b', 2);
+
+console.log(myMap.entries()); // Output: [['a', 1], ['b', 2]]
